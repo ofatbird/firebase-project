5421b39ed23453c1493958510d79e0b12599fa4e
Polly <973367375@qq.com>
2017-08-30 17:39:58 +0800

    update

--- bdfilms/index.js
+++ bdfilms/index.js
@@ -2,17 +2,12 @@ const superagent = require('superagent')
 const cheerio = require('cheerio')
 
 require('superagent-charset')(superagent)
+// 过滤出最近7天的信息, 返回true or false
+function filterRecentSevendays() {
 
-// superagent
-// 	.get('http://www.bd-film.com/rss.jspx')
-// 	.end((err, res) => {
-// 		if (err) return console.log(err)
-// 		const $ = cheerio.load(res.text.replace(/<!\[CDATA\[([\s\S]*?)\]\]>(?=\s*<)/gi, "$1"))
-// 		console.log($('item description').text())
-// 	})
+}
 
 // 第一步搜索RSS来获取单体地址
-
 function getMovieUrl (looplimit, failed) {
   if (!looplimit) return failed(new Error(`Failed to load`))
   return new Promise((resolve, reject) => {
@@ -26,13 +21,12 @@ function getMovieUrl (looplimit, failed) {
 					const $item = $(item)
 					const text = $item.find('title').text()
           const regtitle = /《([\s\S]*?)》/g.exec(text)
-          const description = $item.find('description').text()
-					const pubdate = $item.find('pubDate').text()
-					const title = regtitle ? regtitle[0] : text
           data.push({
-            title,
-            pubdate,
-          description})
+						link: $item.find('guid').text(),
+            title: regtitle ? regtitle[0] : text,
+            pubdate: $item.find('pubDate').text(),
+						description: $item.find('description').text(),
+					})
 				})
 				resolve(data)
       })
