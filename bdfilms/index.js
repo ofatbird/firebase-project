7badde884d1a5baec897b6b8144d49834ec9689e
Chee Tsui <973367375@qq.com>
2017-08-30 21:31:58 +0800

    add functio filter...

--- bdfilms/index.js
+++ bdfilms/index.js
@@ -3,12 +3,13 @@ const cheerio = require('cheerio')
 
 require('superagent-charset')(superagent)
 // 过滤出最近7天的信息, 返回true or false
-function filterRecentSevendays() {
-
+function filterRecentSevendays(pubDate) {
+  const time = new Date(pubDate.split(' ')[0]).getTime()
+  return Date.now() - time < 7 * 24 * 36 * 1E5 ? true : false
 }
 
 // 第一步搜索RSS来获取单体地址
-function getMovieUrl (looplimit, failed) {
+function getMovieUrl(looplimit, failed) {
   if (!looplimit) return failed(new Error(`Failed to load`))
   return new Promise((resolve, reject) => {
     superagent
@@ -18,21 +19,24 @@ function getMovieUrl (looplimit, failed) {
         const $ = cheerio.load(res.text.replace(/<!\[CDATA\[([\s\S]*?)\]\]>(?=\s*<)/gi, '$1'))
         const data = []
         $('item').each((index, item) => {
-					const $item = $(item)
-					const text = $item.find('title').text()
+          const $item = $(item)
+          const text = $item.find('title').text()
           const regtitle = /《([\s\S]*?)》/g.exec(text)
-          data.push({
-						link: $item.find('guid').text(),
-            title: regtitle ? regtitle[0] : text,
-            pubdate: $item.find('pubDate').text(),
-						description: $item.find('description').text(),
-					})
-				})
-				resolve(data)
+          const pubdate = $item.find('pubDate').text()
+          if (filterRecentSevendays(pubdate)) {
+            data.push({
+              pubdate,
+              link: $item.find('guid').text(),
+              title: regtitle ? regtitle[0] : text,
+              description: $item.find('description').text(),
+            })
+          }
+        })
+        resolve(data)
       })
   })
 }
 
 getMovieUrl(10)
-	.then(data => console.log(data))
-	.catch(err => console.log(err))
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
